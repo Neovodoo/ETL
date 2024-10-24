65f5cb0b9139915151b83cba4fdfe2c28a9ec6ce
Rogoza Kirill <75004961+KirillRg@users.noreply.github.com>
2024-10-25 01:56:38 +0300

    Create structure.bash

---
+++ structure.bash
@@ -0,0 +1,17 @@
+ETL/
+├── Final task 1/             # Files for final task
+│   ├── Full Class Diagram for ETL system      
+│   ├── Collaboration Diagrams for all roles      
+│   ├── SIAOUT Table
+│   ├── StoryMap Graphical Representation       
+│   ├── StoryMap Document Description       
+│   ├── Statecharts for each team member
+│   ├── Use Cases Diagrams for all roles
+│   └── UseCases_texts for textual description  
+├── Presentations/            # Folder with project presentations
+├── Task 3/                   # Files for task 3
+├── Task 4/                   # Files for task 4
+├── Task 5/                   # Files for task 5
+├── Task 6/                   # Files for task 6
+├── Task 7/                   # Files for task 7
+└── project_description.md      # Project description
